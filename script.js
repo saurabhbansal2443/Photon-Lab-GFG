6897f8f4d10f8fec735d812bc429dd80b71e39dc
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-12-07 21:31:05 +0530

    Input range features completed

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const fileInput = document.getElementById("fileInput");
+const brightnessInput = document.getElementById("brightness-input");
+const contrastInput = document.getElementById("contrast-input");
+const saturationInput = document.getElementById("saturation-input");
+const blurInput = document.getElementById("blur-input");
 
 let image = new Image();
 
 fileInput.addEventListener("change", function (e) {
   const file = e.target.files[0];
-
   if (!file) return;
-
   const reader = new FileReader();
-
   reader.onload = () => {
     image.src = reader.result;
   };
-
   reader.readAsDataURL(file);
 });
 
@@ -24,3 +24,19 @@ image.onload = () => {
   canvas.width = image.width;
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 };
+
+function applyFilters() {
+  const brightnessValue = brightnessInput.value;
+  const contrastValue = contrastInput.value;
+  const saturationValue = saturationInput.value;
+  const blurValue = blurInput.value;
+
+  ctx.filter = `brightness(${brightnessValue}%)contrast(${contrastValue}%)saturate(${saturationValue}%)blur(${blurValue}px)`;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
+
+brightnessInput.addEventListener("input", applyFilters);
+contrastInput.addEventListener("input", applyFilters);
+saturationInput.addEventListener("input", applyFilters);
+blurInput.addEventListener("input", applyFilters);
